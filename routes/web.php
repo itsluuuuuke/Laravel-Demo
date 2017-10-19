4fc1df652173f7f4c35a11fcc8f7a958aeb983e4
itsluke <624672058@qq.com>
2017-10-19 16:03:05 +0800

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//First Routing demo --Required parameters
+Route::get('ID/{id}', function($id){
+	echo 'id = ' . $id;
+});
+
+//Second Routing demo --Optional parameters
+//Try: http://127.0.0.1/user
+//or http://127.0.0.1/user/anotherUser
+Route::get('/user/{name?}', function($name = 'Luke'){
+	echo 'username: ' . $name; 
+});
+
+//Route redirect demo
+Route::redirect('/redirect','/',301);
+
